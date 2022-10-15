3c39b19eebb0f195c04ae7c35c32ed6e79352f96
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2022-10-15 21:27:54 +0600

    adding appoitment page and react day picker

--- src/pages/Appointment/Appointment.js
+++ src/pages/Appointment/Appointment.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import chair from '../../assets/images/chair.png'
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+import { format } from 'date-fns';
 
 const Appointment = () => {
+
+    const [date, setDate] = useState(new Date());
+
+
     return (
-        <div>
-            <h1>this is Appointment</h1>
+        <div className="hero-bg hero min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt='deantace chair' />
+                <div>
+                    <DayPicker
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                    // footer={footer}
+                    />
+                    <h5>you have selected date {format(date, 'PP')}</h5>
+                </div>
+            </div>
         </div>
     );
 };
